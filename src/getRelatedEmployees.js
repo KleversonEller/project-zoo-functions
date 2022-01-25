a1ec7b05e3ed48b8148b26bc8db049eb122cd045
Kleverson Eller <kleverson.a16@gmail.com>
2022-01-25 19:30:06 -0300

    Requisito - 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  return employees.some(({ managers }) => managers.some((valor) => valor === id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const colaboradores = employees.filter(({ managers }) => managers
+      .some((valor) => valor === managerId));
+    const result = [];
+    colaboradores.forEach(({ firstName, lastName }) => result
+      .push(`${firstName} ${lastName}`));
+    return result;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
