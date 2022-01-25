8981f2157d3129eddf2a2c01e5218c5f8ab50989
Kleverson Eller <kleverson.a16@gmail.com>
2022-01-25 18:16:21 -0300

    Requisito - 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName !== undefined) {
+    return employees.find(({ firstName, lastName }) =>
+      employeeName === firstName || employeeName === lastName);
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
