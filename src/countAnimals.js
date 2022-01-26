fa0567a62e742c9470ab5f75f5e7653cb06daf3a
Kleverson Eller <kleverson.a16@gmail.com>
2022-01-25 21:45:05 -0300

    Requisito - 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
+  let resultado;
+  if (animal === undefined) {
+    resultado = {};
+    species.forEach(({ name, residents }) => {
+      resultado[name] = residents.length;
+    });
+    return resultado;
+  }
+  species.find(({ name, residents }) => {
+    if (name === animal.specie && animal.sex !== undefined) {
+      resultado = residents.filter(({ sex }) => sex === animal.sex).length;
+    } else if (name === animal.specie) {
+      resultado = residents.length;
+    }
+    return resultado;
+  });
+  return resultado;
 }
 
 module.exports = countAnimals;
