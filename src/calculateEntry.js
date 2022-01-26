98b72a3fd08ccb4cdceea6162cb3ef364563eb52
Kleverson Eller <kleverson.a16@gmail.com>
2022-01-25 23:26:34 -0300

    Requisito - 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
+const quantidadePessoas = (min, max, objeto) => objeto.reduce((quantidade, { age }) => {
+  if (age >= min && age < max) {
+    return quantidade + 1;
+  }
+  return quantidade;
+}, 0);
+
 function countEntrants(entrants) {
   // seu código aqui
+  return {
+    child: quantidadePessoas(0, 18, entrants),
+    adult: quantidadePessoas(18, 50, entrants),
+    senior: quantidadePessoas(50, 500, entrants),
+  };
 }
 
-function calculateEntry(entrants) {
+function calculateEntry(entrants = 0) {
   // seu código aqui
+  if (entrants === 0 || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const objeto = countEntrants(entrants);
+  const keys = Object.keys(objeto);
+  const values = Object.values(objeto);
+  return keys.reduce((valorTotal, valorUnitario, index) => {
+    const valor = (prices[valorUnitario] * values[index]);
+    return valorTotal + valor;
+  }, 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
